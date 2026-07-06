1cccb136dd1feb7caea751e6c08c2c3c2548a40c
MarkusTM86 <markus.david@tekniskamuseet.se>
2026-07-06 15:14:41 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,3 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("x:")
-    basic.showNumber(x)
-    basic.showString("y:")
-    basic.showNumber(y)
-    specialblock.korHem(x, y)
+basic.forever(function () {
+
 })
-input.onButtonPressed(Button.A, function () {
-    finch.setMove(MoveDir.Forward, 5, 20)
-    if (specialblock.pekarMot(KoordinatRiktning.Oster)) {
-        x += 5
-    }
-    if (specialblock.pekarMot(KoordinatRiktning.Norr)) {
-        y += 5
-    }
-    if (specialblock.pekarMot(KoordinatRiktning.Vaster)) {
-        x += -5
-    }
-    if (specialblock.pekarMot(KoordinatRiktning.Soder)) {
-        y += -5
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    specialblock.svangVanster()
-})
-let y = 0
-let x = 0
-finch.startFinch()
-specialblock.pekaMotOster()
